16ff2ac33bfc9bf8a64a16c4028b4927b6ed8140
Erick <erick.barross@hotmail.com>
2023-02-12 16:18:05 -0300

    first commit

---
+++ componentes/botaoDelete.js
@@ -0,0 +1,17 @@
+const botaoDelete = ()=> {
+    const botaoDelete = document.createElement('button')
+    
+        botaoDelete.classList = ('delete-button')
+        botaoDelete.innerText = 'deletar'
+        botaoDelete.addEventListener('click', deletarTarefa)
+    
+        return botaoDelete
+    }
+    const deletarTarefa = (evento) => {
+        const botaoDelete = evento.target
+        const tarefaCompleta = botaoDelete.parentElement
+        tarefaCompleta.remove()
+        
+        return botaoDelete
+    }
+    export default botaoDelete
---
+++ main.js
@@ -0,0 +1,33 @@
+import botaoConclui from './componentes/concluirTarefa.js'
+import botaoDelete from './componentes/botaoDelete.js'
+
+const criarTarefa = (evento) => {
+    
+    evento.preventDefault()
+    const lista = document.querySelector('[data-list]')
+    const input = document.querySelector('[data-form-input]')
+    const valor = input.value
+
+
+
+    const tarefa = document.createElement('li')
+    tarefa.classList.add('task')
+    const conteudo = `<p class="content">${valor}</p>`
+    
+    tarefa.innerHTML = conteudo
+    
+    tarefa.appendChild(botaoConclui())
+    tarefa.appendChild(botaoDelete())
+    lista.appendChild(tarefa)
+
+    input.value = " " 
+
+}
+
+const novaTarefa = document.querySelector('[data-form-button]')
+    
+novaTarefa.addEventListener('click', criarTarefa)
+
+
+
+
